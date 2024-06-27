a75c3f26703f3aaa73022c30f416fc6774c7e6e3
Armando Antonio Navarro Flores <armandonavarro@MacBook-Air-de-Armando.local>
2024-06-27 12:49:46 -0700

    task 8 version 1

--- final_project/index.js
+++ final_project/index.js
@@ -13,11 +13,11 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 app.use("/customer/auth/*", function auth(req,res,next){
     if (req.session.authorization) {
         let token = req.session.authorization['accessToken'];
-        // Verify JWT token
+        
         jwt.verify(token, "access", (err, user) => {
             if (!err) {
                 req.user = user;
-                next(); // Proceed to the next middleware
+                next(); 
             } else {
                 return res.status(403).json({ message: "User not authenticated" });
             }
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,13 +6,26 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const username = req.body.username;
+  const password = req.body.password;
+if(!username && !password){
+  return res.status(404).json({message : "Please type an username and/or a password"})
+}
+
+  
+  if (username && password) {
+    if (isValid(username)) {
+      users.push({ "username": username, "password": password });
+      return res.status(200).json({ message: "User successfully registered. Now you can login" });
+    } else {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+  }
+  return res.status(404).json({ message: "Unable to register user." });
 });
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-  //Write your code here
   return res.send(JSON.stringify(books))
 });
 
